9c97066afcabb91ac64b462dc6dfb478dd0a6ad2
pCYSl5EDgo <pcysl5edgo@yahoo.co.jp>
2019-04-07 04:41:17 +0900

    [update]fixed sbyte[64]をulongに変更

--- Unsafe/RemoveCards8x8.cs
+++ Unsafe/RemoveCards8x8.cs
@@ -5,66 +5,114 @@ using Unity.Collections.LowLevel.Unsafe;
 
 namespace AddAndBanish.LowLevel.Unsafe
 {
-    public unsafe struct RemoveCards8x8 : IRemoveCards
+    public unsafe struct RemoveCards8x8 : IRemoveCards<Board8x8>
     {
-        internal int Width, Height, Sum, Count;
-        internal fixed sbyte Values[64];
-        internal ref sbyte this[int x, int y] => ref Values[(x << 3) + y];
-        sbyte IRemoveCards.this[int x, int y] => Values[(x << 3) + y];
+        private const int _Width = 8;
+        private const int _Height = 8;
+        private const int _Length = 64;
+        internal int Sum, Count;
+        internal ulong Cards;
 
-        public int Length => Width * Height;
-        int IRemoveCards.Width => Width;
-        int IRemoveCards.Height => Height;
+        public int Length => _Length;
+        public int Width => _Width;
+        public int Height => _Height;
         int IRemoveCards.Count => Count;
         int IRemoveCards.Sum => Sum;
 
-        public bool IsHamilton => throw new NotImplementedException();
-        public bool IsValid(int goal) => Sum == goal && IsHamilton;
+        public bool IsHamilton => this.IsHamilton();
 
-        IRemoveCards IRemoveCards.Add(int x, int y, sbyte number) => Add(x, y, number);
-        public RemoveCards8x8 Add(int x, int y, sbyte number)
+        public bool this[int x, int y]
         {
-            var answer = this;
-            if (x >= answer.Width)
-                answer.Width = x + 1;
-            if (y >= answer.Height)
-                answer.Height = y + 1;
-            answer.Sum += (answer[x, y] = number);
-            answer.Count++;
-            return answer;
+            get => ((Cards >> ((x << 3) + y)) & 1) == 1;
+            internal set => Cards ^= (value ? 1u : 0u) << ((x << 3) + y);
         }
 
-        public bool DoesExist(int x, int y) => this[x, y] != CalcIndexHelper.NOT_REMOVE_CARD_NUMBER;
-
-        public bool Equals(in RemoveCards8x8 other)
-        {
-            if (Count != other.Count || Sum != other.Count || Width != other.Width || Height != other.Height) return false;
-            fixed (sbyte* thisPtr = this.Values)
-            fixed (sbyte* otherPtr = other.Values)
-            {
-                return UnsafeUtility.MemCmp(thisPtr, otherPtr, Width << 3) == 0;
-            }
-        }
-
-        bool IEquatable<IRemoveCards>.Equals(IRemoveCards other)
+        public bool Equals(in RemoveCards8x8 other) => Cards == other.Cards && Sum == other.Sum && Count == other.Count;
+        public bool Equals(IRemoveCards other)
         {
             if (other is null) return false;
             if (other is RemoveCards8x8 other8x8) return Equals(other8x8);
-            if (Count != other.Count || Sum != other.Count || Width != other.Width || Height != other.Height) return false;
-            for (int x = 0; x < Width; x++)
-                for (int y = 0; y < Height; y++)
+            int width = other.Width, height = other.Height;
+            if (Count != other.Count || Sum != other.Count || _Width != width || _Height != height) return false;
+            for (int x = 0; x < width; x++)
+                for (int y = 0; y < height; y++)
                     if (this[x, y] != other[x, y])
                         return false;
             return true;
         }
 
-        public int GetConnection(int x, int y) => (x > 0 && DoesExist(x - 1, y) ? 1 : 0) + (x < Width - 1 && DoesExist(x + 1, y) ? 1 : 0) + (y > 0 && DoesExist(x, y - 1) ? 1 : 0) + (y < Height - 1 && DoesExist(x, y + 1) ? 1 : 0);
+        public bool Equals(IRemoveCards<Board8x8> other)
+        {
+            if (other is null) return false;
+            if (other is RemoveCards8x8 other8x8) return Equals(other8x8);
+            if (Count != other.Count || Sum != other.Count || _Width != other.Width || _Height != other.Height) return false;
+            for (int x = 0; x < _Width; x++)
+                for (int y = 0; y < _Height; y++)
+                    if (this[x, y] != other[x, y])
+                        return false;
+            return true;
+        }
 
-        public Generics.RemoveCardsEnumerator<RemoveCards8x8> GetEnumerator() => new Generics.RemoveCardsEnumerator<RemoveCards8x8>(this);
+        int IRemoveCards.GetConnection(int x, int y) => this.GetConnection(x, y);
+
+        public Generics.RemoveCardsEnumerator<Board8x8, RemoveCards8x8> GetEnumerator() => new Generics.RemoveCardsEnumerator<Board8x8, RemoveCards8x8>(this);
         IEnumerator<(int x, int y)> IEnumerable<(int x, int y)>.GetEnumerator() => GetEnumerator();
         IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
 
-        Generics.NeighborEnumerable<Board8x8, RemoveCards8x8> GetNeighborEnumerable(in Board8x8 board, int goal) => new Generics.NeighborEnumerable<Board8x8, RemoveCards8x8>(board, this, goal);
-        IEnumerable<Scard> IRemoveCards.GetNeighborEnumerable(IBoard board, int goal) => board is Board8x8 board8x8 ? (IEnumerable<Scard>)GetNeighborEnumerable(board8x8, goal) : (IEnumerable<Scard>)new Generics.NeighborEnumerable<IBoard, RemoveCards8x8>(board, this, goal);
+        public Generics.NeighborEnumerable<Board8x8, RemoveCards8x8> GetNeighborEnumerable(in Board8x8 board, int goal) => new Generics.NeighborEnumerable<Board8x8, RemoveCards8x8>(board, this, goal);
+        IEnumerable<Scard> IRemoveCards.GetNeighborEnumerable(IBoard board, int goal)
+        => board is null ? throw new ArgumentNullException() : board is Board8x8 board8x8 ? (IEnumerable<Scard>)GetNeighborEnumerable(board8x8, goal) : new Generics.NeighborEnumerable<IBoard, RemoveCards8x8>(board, this, goal);
+
+        public Board8x8 CalcNextStepByRemove(ref Board8x8 board)
+        {
+            var builder = new BoardBuilder_MUST_BE_DISPOSED();
+            try
+            {
+                builder.FromBoard(ref board);
+                foreach (var (x, y) in this)
+                {
+                    builder.Remove(x, y);
+                    if (y == 0 && !builder.DoesExist(x, 0))
+                        builder.RemoveColumn(x);
+                }
+                builder.Shrink();
+                return builder.ToBoard8x8();
+            }
+            finally
+            {
+                builder.Dispose();
+            }
+        }
+        Board8x8 IRemoveCards<Board8x8>.CalcNextStepByRemove(Board8x8 board) => CalcNextStepByRemove(ref board);
+
+        public RemoveCards8x8 Add(int x, int y, sbyte number)
+        {
+            var answer = this;
+            if (number == CalcIndexHelper.NOT_REMOVE_CARD_NUMBER)
+            {
+                if (answer[x, y])
+                {
+                    answer[x, y] = false;
+                    answer.Count--;
+                    answer.Sum -= number;
+                }
+            }
+            else
+            {
+                if (!answer[x, y])
+                {
+                    answer[x, y] = true;
+                    answer.Count++;
+                    answer.Sum += number;
+                }
+            }
+            return answer;
+        }
+
+        IRemoveCards<Board8x8> IRemoveCards<Board8x8>.Add(int x, int y, sbyte number) => Add(x, y, number);
+
+        IRemoveCards IRemoveCards.Add(int x, int y, sbyte number) => Add(x, y, number);
+
+        public bool IsValid(int goal) => Sum == goal && IsHamilton;
     }
 }
