cde7ec412b6c4e63614d767e012ee8d9adf4dc84
pCYSl5EDgo <pcysl5edgo@yahoo.co.jp>
2019-04-07 01:12:28 +0900

    [update]Is_AnyOf4NeighborCards_RemoveCardにジェネリクス版を追加定義

--- RemoveCardsHelper.cs
+++ RemoveCardsHelper.cs
@@ -10,5 +10,11 @@ namespace AddAndBanish
         (x + 1 < removeCards.Width && removeCards.DoesExist(x + 1, y)) ||
         (y > 0 && removeCards.DoesExist(x, y - 1)) ||
         (y + 1 < removeCards.Height && removeCards.DoesExist(x, y + 1));
+
+        internal static bool Is_AnyOf4NeighborCards_RemoveCard<T>(this ref T removeCards, int x, int y) where T : struct, IRemoveCards
+        => (x > 0 && removeCards.DoesExist(x - 1, y)) ||
+        (x + 1 < removeCards.Width && removeCards.DoesExist(x + 1, y)) ||
+        (y > 0 && removeCards.DoesExist(x, y - 1)) ||
+        (y + 1 < removeCards.Height && removeCards.DoesExist(x, y + 1));
     }
 }
